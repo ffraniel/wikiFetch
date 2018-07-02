e8de6beb334ba7a9229c3026c4e43dec74ebd3cd
ffraniel <francis.whitehead@googlemail.com>
2018-07-02 15:23:47 +0100

    fetch tweaked

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import List from './List.js';
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state={
       input:"",
-      results:[]
+      results:{
+        names:[],
+        details:[],
+        links:[]
+      }
     }
     this.handleChange = this.handleChange.bind(this);
     this.fetchWiki = this.fetchWiki.bind(this);
@@ -16,6 +21,15 @@ class App extends Component {
     this.setState({
       input:e.target.value
     })
+    if(this.state.input.length === 0) {
+      this.setState({
+        results:{
+          names:[],
+          details:[],
+          links:[]
+        }  
+      })
+    }
     this.fetchWiki(e.target.value);
   }
 
@@ -24,20 +38,32 @@ class App extends Component {
     return fetch(searchURL)
       .then(resbuffer=> resbuffer.json())
       .then(res=>{
+        let fixedRes;
+        res[1] !== undefined ? 
+        fixedRes = {
+          names:res[1],
+          details:res[2],
+          links:res[3]
+        } :
+        fixedRes = {
+            names:[],
+            details:[],
+            links:[]
+          }          
         this.setState({
-          results:res
+          results:fixedRes
         })
       })
       .catch(console.log)
-
   }
 
   render() {
     return (
       <div className="App">
-        <h4>Wikipedi Live Search</h4>
+        <h4 className="title">Wikipedi Live Search</h4>
         <input type="text" value={this.state.input} onChange={this.handleChange} />
         {this.state.input && <p>You are searching for {this.state.input}</p>}
+        <List results={this.state.results} />
       </div>
     );
   }
---
+++ src/List.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import "./List.css";
+
+export default class List extends Component {
+  render() {
+    return (
+      <div className="List">
+        {this.props.results.names.length === 0 && <p>Type something in to see some search results</p>}
+        {this.props.results.names.length > 1 &&
+          this.props.results.names.map((value, key) => {
+            var link = this.props.results.links[key];
+            var detail = this.props.results.details[key];
+
+            return (
+              <div className="search-result-item" key={key}>
+                <a href={link}>
+                  <h3>{value}</h3>
+                </a>
+                <p>{detail}</p>
+              </div>
+            );
+          })}
+          {/* <p></p>
+          <button></button> */}
+      </div>
+    );
+  }
+}
